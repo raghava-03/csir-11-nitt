ed63b545d26d2ff6f0b1847ae863fb1d09668e50
raghava-03 <143477854+raghava-03@users.noreply.github.com>
2023-09-12 13:36:32 +0530

    Add files via upload

---
+++ c programs.cpp
@@ -0,0 +1,11 @@
+/*first program in c*/
+#include<stdio.h>
+int main()
+{
+	printf("enter two numbers for addition");
+	float a,b,c;
+	scanf("%f %f",&a,&b);
+	c=a+b;
+	printf("the sum of %f and %f is \n %f",a,b,c);
+	return 0;
+}
